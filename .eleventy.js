79b9dabffe5a2b4289c1694ac83384b1ffc65de5
Vyacheslav Dobranov <vy.dobranov@yandex.ru>
2026-04-18 14:23:16 +0500

    Changes to be committed: 	new file:   .eleventy.js 	modified:   .gitignore 	new file:   _site/about.html 	new file:   _site/contacts.html 	new file:   _site/css/style.css 	new file:   _site/cv.html 	new file:   _site/developments/ifc-ids-report/index.html 	new file:   _site/img/needles.PNG 	renamed:    img/photo.png -> _site/img/photo.png 	new file:   _site/index.html 	deleted:    css/style.css 	deleted:    index.html 	new file:   src/_includes/footer.njk 	new file:   src/_includes/header.njk 	new file:   src/_includes/layout.njk 	new file:   src/about.njk 	new file:   src/contacts.njk 	new file:   src/css/style.css 	new file:   src/cv.njk 	new file:   src/developments/ifc-ids-report.md 	new file:   src/img/needles.PNG 	new file:   src/img/photo.png 	new file:   src/index.njk

---
+++ .eleventy.js
@@ -0,0 +1,38 @@
+const markdownIt = require('markdown-it');
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('src/css/');
+  eleventyConfig.addPassthroughCopy('src/img/');
+
+  const md = markdownIt({
+    html: true,
+    linkify: true,
+    typographer: true
+  });
+
+  eleventyConfig.setLibrary('md', md);
+
+  eleventyConfig.addCollection('developments', function(collectionApi) {
+    return collectionApi.getFilteredByGlob('src/developments/*.md').sort((a, b) => {
+      return new Date(b.date) - new Date(a.date);
+    });
+  });
+
+  eleventyConfig.addCollection('posts', function(collectionApi) {
+    return collectionApi.getFilteredByGlob('src/posts/*.md').sort((a, b) => {
+      return new Date(b.date) - new Date(a.date);
+    });
+  });
+
+  return {
+    dir: {
+      input: 'src',
+      output: '_site',
+      includes: '_includes',
+      data: '_data'
+    },
+    templateFormats: ['njk', 'md', 'html'],
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk'
+  };
+};
---
+++ src/developments/ifc-ids-report.md
@@ -0,0 +1,19 @@
+---
+title: "Автоматический отчёт по валидации IFC"
+date: 2024-01-15
+description: "Автоматический отчёт по валидации IFC моделей"
+layout: layout.njk
+---
+
+<div class="left-column">
+  <nav class="sidebar">
+    <li><a href="/developments/ifc-ids-report/">IFC IDS Report</a></li>
+  </nav>
+</div>
+<div class="divider"></div>
+<div class="right-column">
+  <div class="content">
+    <h1>Автоматический отчёт по валидации IFC</h1>
+    <p>Описание разработки...</p>
+  </div>
+</div>
