2b55d53a46321e87a9e8dcb9e7d4c3f14e8224eb
Vyacheslav Dobranov <vy.dobranov@yandex.ru>
2026-04-22 15:38:53 +0500

    Changes to be committed: 	modified:   .eleventy.js 	modified:   _site/css/style.css 	modified:   src/css/style.css 	new file:   src/fonts/IosevkaTermSlab-Bold.woff2 	new file:   src/fonts/IosevkaTermSlab-Italic.woff2 	new file:   src/fonts/IosevkaTermSlab-Regular.woff2

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("src/favicon.svg");
 	eleventyConfig.addPassthroughCopy("src/css/");
 	eleventyConfig.addPassthroughCopy("src/img/");
+	eleventyConfig.addPassthroughCopy("src/fonts/");
 	eleventyConfig.addPassthroughCopy("src/developments/*.png");
 	eleventyConfig.addPassthroughCopy("src/posts/*.png");
 
